909bd32363fb6c6a76ffcec6e4c2f1529bc29af2
Danilo Romano <risadex@gmail.com>
2022-05-07 09:11:49 -0300

    added first mtc_vpc
    Signed-off-by: Danilo Romano <risadex@gmail.com>

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = "sa-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  region      = "sa-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "vscode"
 }
